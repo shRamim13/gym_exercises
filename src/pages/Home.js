5cf3388500c57be2fbc77268759b2621e69b56d6
shRamim13 <shramim13@gmail.com>
2023-11-19 11:40:21 +0600

    Herobanner

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,16 @@
 import React from 'react'
-
+import { useState } from 'react'
+import { Box } from '@mui/material'
+import HeroBanner from '../Component/HeroBanner'
+import SearchExercises from '../Component/SearchExercises'
+import Exercises from '../Component/Exercises'
 const Home = () => {
     return (
-        <div>Home</div>
+        <Box>
+            <HeroBanner />
+            <SearchExercises />
+            <Exercises />
+        </Box>
     )
 }
 
