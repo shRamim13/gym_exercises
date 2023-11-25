926b542a1549439ebb856117ff14e4d01aa00dfe
shRamim13 <shramim13@gmail.com>
2023-11-26 02:02:22 +0600

    last commit on fitness App

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,26 +1,50 @@
+
 export const exerciseOptions = {
     method: 'GET',
     headers: {
-        'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
-        'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
-    }
-};
-
-
-export const fetchData = async (url, options) => {
-    const response = await fetch(url, options);
-    const data = await response.json();
+      'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+      'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+    },
+  };
+  
+  export const youtubeOptions = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+      'X-RapidAPI-Key': '4f887d0f94mshb0d53aefee8cc98p1efbefjsn451bca5de830',
+    },
+  };
+  
+  export const fetchData = async (url, options) => {
+    const res = await fetch(url, options);
+    const data = await res.json();
+  
     return data;
-}
-
-
+  };
+  
 // export const exerciseOptions = {
 //     method: 'GET',
 //     headers: {
-//       'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
-//       'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
-//     },
-//   };
+//         'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+//         'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+//     }
+// };
+
+
+// export const fetchData = async (url, options) => {
+//     const response = await fetch(url, options);
+//     const data = await response.json();
+//     return data;
+// }
+
+
+// // export const exerciseOptions = {
+// //     method: 'GET',
+// //     headers: {
+// //       'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+// //       'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+// //     },
+// //   };
   
 //   export const youtubeOptions = {
 //     method: 'GET',
